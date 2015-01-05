259a3c867fc4cb5e1c00334d36a63392adce4a75
dev_menumap <dev@menumap.co.kr>
2015-01-06 02:21:16 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MenuMap::Application.config.secret_key_base = 'e3769776775feac7431b0de65fe9f9edf6ae74e96b0fae394c520eeb0f11e99fff76e92ebd367db2af13fb62ac8ac3d0704ce25c43c36f9f483b1be58ad6c1f9'
