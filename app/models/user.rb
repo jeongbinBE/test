30dcb878743198069a9f8c598bd0fac441140bef
dev_menumap <dev@menumap.co.kr>
2015-01-10 18:24:28 +0900

    create user and email columns with basic validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ActiveRecord::Base
+
+	# before save func.
+	before_save { self.username = username.downcase }
+	before_save { self.email = email.downcase }
+	
+	# Basic validations
+	validates :username, 
+						:presence => { :message => "MenuMap에서 사용하실 아이디를 적어주세요."},
+						:length   => { :minimum => 3, :maximum => 25,
+													 :too_short => "3자 이상의 아이디를 사용해주세요.",
+													 :too_long => "25자 이하의 아이디만 가능합니다." },
+						:uniqueness => { :case_sensitive => false,
+														 :message => "이미 사용중인 아이디입니다." }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email,
+						:presence => { :message => "로그인과 회원가입에 사용하실 이메일을 적어주세요."},
+						:format => { :with    => VALID_EMAIL_REGEX,
+												 :message => "이메일 형식이 올바르지 않습니다." },
+						:uniqueness => { :case_sensitive => false, 
+														 :message => "이미 사용중인 이메일입니다." }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,69 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	
+	def setup
+		@user = User.new(username: "TestUser", email: "TestUser@menumap.co.kr")
+	end
+
+	test "user should be valid" do
+		assert @user.valid?
+	end
+
+	# Username
+	test "username should be present" do
+		@user.username = "  "
+		assert_not @user.valid?
+	end
+
+	test "username should not be too short" do
+		@user.username = "aa"
+		assert_not @user.valid?
+	end
+
+	test "username should not be too long" do
+		@user.username = "a" * 26 
+		assert_not @user.valid?
+	end
+
+	test "username sholud be unique" do
+		duplicate = @user.dup
+		duplicate.email = "temp@menumap.co.kr"
+		duplicate.username.upcase!
+		@user.save
+		assert_not duplicate.valid?
+	end
+
+	# Email
+	test "email should be present" do
+		@user.email = ""
+		assert_not @user.valid?
+	end
+
+  test "here are valid addresses" do
+    valid_addresses = %w[test@valuemap.com test@menumap.co.kr USER@google.COM 
+												 A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "here are invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+	test "email sholud be unique" do
+		duplicate = @user.dup
+		duplicate.username = "gosss"
+		duplicate.email.upcase!
+		@user.save
+		assert_not duplicate.valid?
+	end
+
 end
