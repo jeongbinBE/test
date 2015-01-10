caaf8d70b4d604a6a19254ec36e5d49d1613f3f3
dev_menumap <dev@menumap.co.kr>
2015-01-10 16:53:50 +0900

    change column name to username and modify timezone

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20150108062402) do
 
   create_table "users", force: true do |t|
-    t.string   "name"
+    t.string   "username"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
