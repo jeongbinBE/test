1a9c66409e1f60ebee9c53ea39c14c49c89db73a
dev_menumap <dev@menumap.co.kr>
2015-01-19 10:23:23 +0900

    Make posts page look well~

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,2 +1,26 @@
 module RestaurantsHelper
+
+	def short_addr(addr)
+		addr.split.take(3).pop(2).join(" ")
+	end
+
+	def css_on_menu(arg)
+		if arg == 11
+			"success"
+		elsif arg == 1
+			"warning"
+		else
+			"default"
+		end
+	end
+
+	def words_on_menu(arg)
+		if arg == 11
+			"메뉴등록 완료"
+		elsif arg == 1
+			"메뉴 등록중"
+		else
+			"메뉴 미등록"
+		end
+	end
 end
