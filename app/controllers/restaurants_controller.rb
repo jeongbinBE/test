66312d8260b8b5849a6bbbcdaac3dcac6dd9780a
dev_menumap <dev@menumap.co.kr>
2015-01-27 10:07:28 +0900

    initial contents for no search results

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,10 +5,11 @@ class RestaurantsController < ApplicationController
   def index
 		@search_name = params[:name]
 		@search_addr = params[:addr]
-		@search_cat  = params[:sub_category]
-		@search_deliver = params[:delivery]
+		@search_cat = Category.find_by(id: params[:category]).name
+		@search_sub_cat  = params[:sub_category]
+		@search_delivery = params[:delivery]
 
-		@restaurants = Restaurant.joins(:rest_key).merge(RestKey.search(@search_deliver, @search_cat, @search_name, @search_addr)).paginate(:page => params[:page]).order('menu_on DESC')
+		@restaurants = Restaurant.joins(:rest_key).merge(RestKey.search(@search_delivery, @search_sub_cat, @search_name, @search_addr)).paginate(:page => params[:page]).order('menu_on DESC')
   end
 
   def show
