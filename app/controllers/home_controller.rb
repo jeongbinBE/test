f6e182fe1188b508e5db313636d7fb5592cc2a70
dev_menumap <dev@menumap.co.kr>
2015-01-30 02:24:35 +0900

    posts instruction on home index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,17 @@ class HomeController < ApplicationController
 	include ActionView::Helpers::NumberHelper
 
   def index
+		
+		# instruction for search
 		@cat = Category.all
 		@sub = SubCategory.all
+
+		# instruction for posts
+		@restaurants = Restaurant.where("id = ? OR id = ? OR id = ?", 
+																		 1000001, 1000234, 1158881)
+
+		# instruction for page
+
 
 		# for meta description
 		@page_description = "메뉴맵은 온라인 메뉴제공 서비스로서 편리하고 객관적인 음식점, 맛집 검색 엔진입니다. 현재 서울시내 #{number_with_delimiter(Restaurant.count, delimiter: ',')}개 음식점의 음식분류, 주소, 전화번호, 배달여부와 #{number_with_delimiter(Menu.count, delimiter: ',')}여개의 메뉴정보를 갖고 있으며, 소비자들이 직접 음식점 정보를 저장할 수 있습니다."
