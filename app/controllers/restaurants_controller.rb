afc268f903e9b5bb71060424eafded6258c48500
dev_menumap <dev@menumap.co.kr>
2015-01-29 21:25:01 +0900

    add dynamic titles

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,13 +9,30 @@ class RestaurantsController < ApplicationController
 		@search_sub_cat  = params[:sub_category]
 		@search_delivery = params[:delivery]
 
+		# search algorithm
 		@restaurants = Restaurant.joins(:rest_key).merge(RestKey.search(@search_delivery, @search_sub_cat, @search_name, @search_addr)).paginate(:page => params[:page]).order('menu_on DESC')
+
+		# for title
+		@page_title = "#{ @search_addr + "주변 " unless @search_addr.empty?}"
+		if @search_name.empty?
+			@page_title += @search_sub_cat 
+		else
+			@page_title += "#{@search_name}(#{@search_sub_cat})"	 		
+		end
+		@page_title += " 배달가능업소" if @search_delivery
+		@page_title += " 검색결과 || MenuMap"
   end
 
   def show
 		@restaurant = Restaurant.find(params[:id])
 		@titles = @restaurant.menu_titles
 		@menus = @restaurant.menus
+
+		# for title
+		@page_title = @restaurant.rest_info.title_addr + " " + @restaurant.name
+		@page_title += "(" + @restaurant.sub_cat
+		@page_title += " 배달가능" if @restaurant.delivery
+		@page_title += ") || MenuMap"
   end
 
 	def update
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# returns full titles.
+	def full_title(page_title = '')
+		base_title = "MenuMap || 온라인 메뉴제공 서비스"
+		if page_title.empty?
+			@page_title || base_title
+		else
+			"#{ page_title } || MenuMap 온라인 메뉴제공 서비스"
+		end
+	end
 end
