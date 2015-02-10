7121e19dda081f9d0c61e7f9bd35a6aeb64735f9
dev_menumap <dev@menumap.co.kr>
2015-02-10 21:52:18 +0900

    make image size limit

--- app/models/add_rest_request.rb
+++ app/models/add_rest_request.rb
@@ -3,8 +3,17 @@ class AddRestRequest < ActiveRecord::Base
 	validates :real_addr, presence: true
 
 	mount_uploader :add_rest_img, AddRestImageUploader
+	validate :picture_size
 
 	# order for addrequest on admin 
 	default_scope -> { order(created_at: :desc) }
 
+	private
+
+		# set maximum image size.
+		def picture_size
+			if picture.size > 10.megabytes
+				errors.add(:picture, "10MB를 넘는 사진입니다.")
+			end
+		end
 end
--- app/models/report_rest_err.rb
+++ app/models/report_rest_err.rb
@@ -1,4 +1,15 @@
 class ReportRestErr < ActiveRecord::Base
 	belongs_to :restaurant
+
 	mount_uploader :rest_err_img, RestErrImageUploader 
+	validate :picture_size
+
+	private
+
+		# set maximum image size.
+		def picture_size
+			if picture.size > 10.megabytes
+				errors.add(:picture, "10MB를 넘는 사진입니다.")
+			end
+		end
 end
