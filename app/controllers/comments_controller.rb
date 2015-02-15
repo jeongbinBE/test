8921c5bc60e73e4bdcd86c5d3c345a2e22bb2024
dev_menumap <dev@menumap.co.kr>
2015-02-15 17:57:22 +0900

    only admin and correct users can delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+	before_action :correct_user?, only: [:destroy], if: :not_admin_user?
 	
 	def create
 		@comment = Comment.new(comment_params)
@@ -30,5 +31,14 @@ class CommentsController < ApplicationController
 	private
 		def comment_params
 			params.require(:comment).permit(:user_id, :restaurant_id, :contents)
+		end
+
+		def correct_user?
+			@user = Comment.find(params[:id]).user
+			redirect_to :back unless current_user?(@user)
+		end
+
+		def not_admin_user?
+			!current_user.admin?
 		end
 end
