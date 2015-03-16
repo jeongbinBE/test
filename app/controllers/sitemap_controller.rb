4248f62e3fcf6e64c084b8b304141e260d0a6ff9
dev_menumap <dev@menumap.co.kr>
2015-03-17 00:08:32 +0900

    complete sitemap for google

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -2,5 +2,10 @@ class SitemapController < ApplicationController
 	layout nil
 
   def show
+		@static = "http://menumap.co.kr/"
+		@restaurants = Restaurant.where("picture is not null OR menu_on > 0 OR delivery is true OR updated_at > ?", Time.now - 7.day)
+		respond_to do |format|
+			format.xml 
+		end
   end
 end
