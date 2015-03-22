d277757d9334df0afcd9ec08045041dc9836eb3c
dev_menumap <dev@menumap.co.kr>
2015-03-22 17:00:44 +0900

    analytics precompile error

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -1,5 +1,6 @@
-$(document).on 'page:change', ->
+/**$(document).on 'page:change', ->
 	if window._gaq?
 		_gaq.push ['_trackPageview']
 	else if window.pageTracker?
 		pageTracker._trackPageview()
+**/
